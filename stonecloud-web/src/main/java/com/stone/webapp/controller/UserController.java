f9b4437dad3d18b615ff8e5027ce64abf103650c
zhengchanglin <yh.lvdousha@gmail.com>
2017-12-08 17:35:16 +0800

    ajax请求出现未知错误的统一处理

--- stonecloud-web/src/main/java/com/stone/webapp/controller/UserController.java
+++ stonecloud-web/src/main/java/com/stone/webapp/controller/UserController.java
@@ -2,11 +2,15 @@ package com.stone.webapp.controller;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.stone.webapp.model.User;
 import com.stone.webapp.service.UserService;
@@ -15,18 +19,21 @@ import com.stone.webapp.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 	
+	private Logger log = Logger.getLogger(this.getClass());
+	
 	@Autowired
 	private UserService userService;
 	
 	@RequestMapping("/insertUser")
 	@ResponseBody
-	public JSONObject insertUser(){
+	public JSONObject insertUser(HttpServletRequest request) throws Exception{
+		log.info("insertUser");
 		User user = new User();
 		user.setCreated(new Date());
 		user.setUpdated(new Date());
 		user.setName("zhengchanglin");
 		userService.insertUser(user);
-		return new JSONObject();
+		return JSON.parseObject(JSON.toJSONString(user));
 	}
 
 }
--- stonecloud-web/src/main/java/com/stone/webapp/listener/SimpleHandlerExceptionResolver.java
+++ stonecloud-web/src/main/java/com/stone/webapp/listener/SimpleHandlerExceptionResolver.java
@@ -1,5 +1,7 @@
 package com.stone.webapp.listener;
 
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,11 +21,33 @@ public class SimpleHandlerExceptionResolver implements HandlerExceptionResolver 
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object object,
 			Exception exception) {
-		log.error("Catch Exception: ",exception); 
-        Map<String,Object> map = new HashMap<String,Object>();
-		StringPrintWriter strintPrintWriter = new StringPrintWriter();  
-		exception.printStackTrace(strintPrintWriter);  
-        map.put("errorMsg", strintPrintWriter.getString());//将错误信息传递给view  
+		String requestType = request.getHeader("X-Requested-With");
+		if(requestType == null){
+			log.info("simple");
+			log.error("Catch Exception: ",exception); 
+			Map<String,Object> map = new HashMap<String,Object>();
+			StringPrintWriter strintPrintWriter = new StringPrintWriter();  
+			exception.printStackTrace(strintPrintWriter);  
+			map.put("errorMsg", strintPrintWriter.getString());//将错误信息传递给view  
+			
+		}else if(requestType.equals("XMLHttpRequest")){
+			log.info("AJAX");
+			try{
+				InputStream is = request.getInputStream();
+				byte[] b = new byte[1024];
+				int status = is.read(b);
+				
+				while(status != -1){
+					log.info(new String(b));
+					status = is.read(b);
+				}
+				PrintWriter writer = response.getWriter();  
+	            writer.write("500");  
+	            writer.flush(); 
+			}catch(Exception e){
+				log.error(e);
+			}
+		}
         return null;
 	}
 
