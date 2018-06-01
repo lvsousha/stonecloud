ccdfd1b2b2d640473f76a1b3f22a2b424918b370
zhengchanglin <yh.lvdousha@gmail.com>
2018-06-01 14:43:49 +0800

    Apache-ignite

--- stonecloud-web/src/main/java/com/stone/webapp/listener/SimpleRequestBodyAdvice.java
+++ stonecloud-web/src/main/java/com/stone/webapp/listener/SimpleRequestBodyAdvice.java
@@ -11,7 +11,6 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodArgumentResolver;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
 
 import com.alibaba.fastjson.JSONObject;
