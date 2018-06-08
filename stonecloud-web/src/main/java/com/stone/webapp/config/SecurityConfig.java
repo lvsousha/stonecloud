c8565aeabefde1ef61de4cd5efb24d32b5f1e274
zhengchanglin <yh.lvdousha@gmail.com>
2018-06-08 16:05:21 +0800

    去除警告

--- stonecloud-web/src/main/java/com/stone/webapp/config/SecurityConfig.java
+++ stonecloud-web/src/main/java/com/stone/webapp/config/SecurityConfig.java
@@ -3,11 +3,9 @@ package com.stone.webapp.config;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
