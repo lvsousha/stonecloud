927a5aef4238e8d7ac230be7712a9f33b28c22da
zhengchanglin <yh.lvdousha@gmail.com>
2018-06-13 10:55:56 +0800

    使用lombok    简化实体类，避免写setter和getter语句

--- stonecloud-web/src/main/java/com/stone/webapp/model/User.java
+++ stonecloud-web/src/main/java/com/stone/webapp/model/User.java
@@ -6,6 +6,9 @@ import com.baomidou.mybatisplus.annotations.TableName;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Data;
+
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)			//不包含有null值的字段,即字段值为null的转换为json字符串时会被省略
 @TableName("USERS")
 public class User implements Serializable{
@@ -18,52 +21,53 @@ public class User implements Serializable{
 	private String name;
 	private String password;
 	private String email;
+	@JsonProperty("phone")
 	private String phone;
 	private Date created;
 	private Date updated;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	@JsonProperty("phone")					//设置该字段转换为json后的字段名
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public Date getCreated() {
-		return created;
-	}
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-	public Date getUpdated() {
-		return updated;
-	}
-	public void setUpdated(Date updated) {
-		this.updated = updated;
-	}
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getPassword() {
+//		return password;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	@JsonProperty("phone")					//设置该字段转换为json后的字段名
+//	public String getPhone() {
+//		return phone;
+//	}
+//	public void setPhone(String phone) {
+//		this.phone = phone;
+//	}
+//	public Date getCreated() {
+//		return created;
+//	}
+//	public void setCreated(Date created) {
+//		this.created = created;
+//	}
+//	public Date getUpdated() {
+//		return updated;
+//	}
+//	public void setUpdated(Date updated) {
+//		this.updated = updated;
+//	}
 	
 }
---
+++ stonecloud-web/src/main/resources/README.txt
@@ -0,0 +1,2 @@
+lombok:
+	简化实体类，避免写setter和getter语句
